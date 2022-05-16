387c47bdb40cae4f0204b81c37bfe8fa5e873e1a
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-05-16 17:19:04 +0530

    Configure cors ooption,  changed sie limit of the json data, add static route for serving avatars

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const httpErrors = require('http-errors');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.disable('x-powered-by');
 
 app.use(
-  cors({ origin: ['http://localhost:3000', 'https://talky-talk.vercel.app'] })
+  cors({
+    origin: ['http://localhost:3000', 'https://talky-talk.vercel.app'],
+    credentials: true,
+  })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
+app.use(cookieParser());
+
+app.use('/storage', express.static('storage/'));
 
 app.get('/', (req, res) =>
   res
     .status(200)
-    .json({ status: 'ok', statusCode: 200, message: 'Hello from Talky.' })
+    .json({ ok: true, statusCode: 200, statusText: 'Hello from Talky.' })
 );
 
 app.use('/api', require('./routes'));
@@ -26,7 +33,7 @@ app.use((req, res, next) => next(httpErrors.NotFound()));
 app.use((error, req, res, next) => {
   if (error.status === 404)
     return res.status(404).json({
-      status: 'error',
+      ok: false,
       message: error.message || 'Not Found.',
       statusCode: 404,
     });
@@ -34,7 +41,7 @@ app.use((error, req, res, next) => {
     if (error.isJoi) error.status = 422;
 
     return res.status(error.status || 500).json({
-      status: 'error',
+      ok: false,
       statusCode: error.status || 500,
       message: error.message || 'Internal Server Error.',
     });
