927dcb2e6f3ea26eb8704ba6bd8d18b283e7c790
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-05-16 17:25:51 +0530

    Refactor code and add new methods in token service for verifying, storing and finding tokens

--- services/token-service.js
+++ services/token-service.js
@@ -1,25 +1,51 @@
 const util = require('util');
 const JWT = require('jsonwebtoken');
+const RefreshToken = require('../models/token-model');
 
 const signToken = util.promisify(JWT.sign);
+const verifyToken = util.promisify(JWT.verify);
 
 const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET;
 const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET;
 class TokenService {
   generateAccessToken(payload) {
     return signToken(payload, JWT_ACCESS_TOKEN_SECRET, {
-      expiresIn: '1h',
-      issuer: process.env.ROOT_DOMAIN,
+      expiresIn: '1m',
+      issuer: process.env.BASE_URL,
       audience: String(payload.id),
     });
   }
 
   generateRefreshToken(payload) {
     return signToken(payload, JWT_REFRESH_TOKEN_SECRET, {
-      expiresIn: '1y',
-      issuer: process.env.ROOT_DOMAIN,
+      expiresIn: '1h',
+      issuer: process.env.BASE_URL,
       audience: String(payload.id),
     });
+  }
+
+  async storeRefreshToken(token, userId) {
+    return RefreshToken.create({ token, userId });
+  }
+
+  setTokenToCookie(res, key, data) {
+    res.cookie(key, data.token, {
+      maxAge: data.age,
+      httpOnly: true,
+      secure: true,
+    });
+  }
+
+  verifyAccessToken(token) {
+    return verifyToken(token, JWT_ACCESS_TOKEN_SECRET);
+  }
+
+  verifyRefreshToken(token) {
+    return verifyToken(token.JWT_REFRESH_TOKEN_SECRET);
+  }
+
+  async findRefreshToken(token, userId) {
+    return RefreshToken.findOne({ token, userId });
   }
 }
 
--- services/user-service.js
+++ services/user-service.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 class UserService {
   async findUser(filter) {
-    const user = await User.findOne({ filter });
+    const user = await User.findOne(filter);
     return user;
   }
 
